208b55967a5eb792b824857c6446a16d630d95e3
Matteo Altamore <maltamore@deloitte.it>
2022-10-11 22:03:21 +0200

    feat: add raw implementation of mqtt services

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,112 @@
-function App() {
-  return <div>Hello world</div>;
-}
+import { useEffect, useState } from "react";
 
-export default App;
+import Connection from "./Connection";
+
+import {
+  CloseCallback,
+  connect,
+  IClientOptions,
+  MqttClient,
+} from "precompiled-mqtt";
+import { IClientSubscribeOptions } from "mqtt";
+
+export default function App() {
+  const [client, setClient] = useState<MqttClient>();
+  const [connectStatus, setConnectStatus] = useState("Connect");
+  const [isSubbed, setIsSubbed] = useState(false);
+  const [payload, setPayload] = useState({});
+
+  const mqttConnect = (host: string, mqttOption: IClientOptions) => {
+    setConnectStatus("Connecting");
+    setClient(connect(host, mqttOption));
+  };
+
+  useEffect(() => {
+    if (!client) return;
+
+    client.on("connect", () => {
+      setConnectStatus("Connected");
+    });
+    client.on("error", (err) => {
+      console.error("Connection error: ", err);
+      client.end();
+    });
+    client.on("reconnect", () => {
+      setConnectStatus("Reconnecting");
+    });
+    client.on("message", (topic, message) => {
+      const payload = { topic, message: message.toString() };
+      setPayload(payload);
+    });
+    client.on("disconnect", () => {
+      setConnectStatus("Connect");
+    });
+  }, [client]);
+
+  const mqttDisconnect = () => {
+    if (!client) return;
+
+    client.end(true);
+  };
+
+  const mqttSub = (subscription: { topic: string }) => {
+    if (!client) return;
+
+    const { topic } = subscription;
+    const closeCallback: CloseCallback = (error) => {
+      if (error) {
+        console.log("Subscribe to topics error", error);
+        return;
+      }
+      setIsSubbed(true);
+    };
+    const options: IClientSubscribeOptions = { qos: 1 };
+    client.subscribe(topic, options, closeCallback);
+  };
+
+  const mqttUnSub = (subscription: { topic: string }) => {
+    if (!client) return;
+
+    const { topic } = subscription;
+    const closeCallback: CloseCallback = (error) => {
+      if (error) {
+        console.log("Unsubscribe error", error);
+        return;
+      }
+      setIsSubbed(false);
+    };
+    client.unsubscribe(topic, closeCallback);
+  };
+
+  const handleSubscribe = () => {
+    const record = {
+      topic: "sensor/temperature",
+    };
+    mqttSub(record);
+  };
+
+  const handleUnsubscribe = () => {
+    const record = {
+      topic: "sensor/temperature",
+    };
+    mqttUnSub(record);
+  };
+
+  return (
+    <>
+      <Connection
+        connect={mqttConnect}
+        disconnect={mqttDisconnect}
+        connectBtn={connectStatus}
+      />
+      <button type="button" onClick={handleSubscribe}>
+        Subscribe
+      </button>
+      <button type="button" onClick={handleUnsubscribe}>
+        Unsubscribe
+      </button>
+      {isSubbed ? "Subbed" : "Unsubbed"}
+      <div>Payload: {JSON.stringify(payload)}</div>
+    </>
+  );
+}
---
+++ src/Connection.tsx
@@ -0,0 +1,84 @@
+import { IClientOptions } from "mqtt";
+import { useRef } from "react";
+interface ConnectionProps {
+  connect: (host: string, mqttOption: IClientOptions) => void;
+  disconnect: () => void;
+  connectBtn: string;
+}
+
+export default function Connection(props: ConnectionProps) {
+  const { connect, disconnect, connectBtn } = props;
+  const hostRef = useRef<HTMLInputElement>(null);
+  const portRef = useRef<HTMLInputElement>(null);
+  const clientIdRef = useRef<HTMLInputElement>(null);
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleConnect = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const url = `wss://${hostRef.current?.value}:${portRef.current?.value}/`;
+    console.log("url: ", url);
+    const options: IClientOptions = {
+      keepalive: 30,
+      protocolId: "MQTT",
+      protocolVersion: 4,
+      clean: true,
+      reconnectPeriod: 1000,
+      connectTimeout: 30 * 1000,
+      will: {
+        topic: "WillMsg",
+        payload: "Connection Closed abnormally..!",
+        qos: 0,
+        retain: false,
+      },
+      rejectUnauthorized: false,
+    };
+    options.clientId = clientIdRef.current?.value ?? undefined;
+    options.username = usernameRef.current?.value ?? undefined;
+    options.password = passwordRef.current?.value ?? undefined;
+    connect(url, options);
+  };
+
+  const handleDisconnect = () => {
+    disconnect();
+  };
+
+  return (
+    <form
+      onSubmit={handleConnect}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        maxWidth: 350,
+      }}
+    >
+      <input type="text" placeholder="host" name="host" ref={hostRef} />
+      <input type="text" placeholder="port" name="port" ref={portRef} />
+      <input
+        type="text"
+        placeholder="client-id"
+        name="clientid"
+        ref={clientIdRef}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        name="username"
+        ref={usernameRef}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        name="password"
+        ref={passwordRef}
+      />
+
+      <button type="submit">{connectBtn}</button>
+      <button type="button" onClick={handleDisconnect}>
+        Disconnect
+      </button>
+    </form>
+  );
+}
